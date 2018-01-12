01c2e1a85b4f54733da2ba92bfa7b6726b9baab1
William Saidac <sdfsfsds>
2018-01-12 23:56:49 +0100

    terraform problem

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,18 @@ variable "project_name" {}
 variable "aws_region" { default = "eu-west-1" }
 
 # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-provider "aws" { region = "${var.aws_region}" }
+provider "aws" {
+  region = "${var.aws_region}"
+   }
 
 # HEROKU_API_KEY, HEROKU_EMAIL
-provider "heroku" {}
+provider "heroku" {
+}
 
 # Store Terraform state in S3 (this must be prepared in advance)
 terraform {
   backend "s3" {
-    bucket = "wp-terraform-backend"
+    bucket = "wp-terraform-backend-wmcs"
     key = "wp/terraform.tfstate"
     region = "eu-west-1"
   }
